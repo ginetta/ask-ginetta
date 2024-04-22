2c639d720371701fd5eee5390e1b2fb7777436f0
Davy Peter Braun <543614+dheavy@users.noreply.github.com>
2024-04-22 11:44:23 +0200

    WIP

--- deployment/fly.io/index/start-vespa.sh
+++ deployment/fly.io/index/start-vespa.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# Try to set ulimits
-ulimit -n 262144 || echo "Failed to set nofile limit"
-ulimit -u 409600 || echo "Failed to set nproc limit"
-ulimit -c unlimited || echo "Failed to set core limit"
+# Check the current vm.max_map_count
+current_max_map=$(sysctl -n vm.max_map_count)
+required_max_map=262144
 
-# Start Vespa configuration server and services
-if [ "$1" == "configserver" ]; then
-  echo "Starting Vespa Config Server..."
-  /opt/vespa/bin/vespa-start-configserver
-elif [ "$1" == "services" ]; then
-  echo "Starting Vespa Services..."
-  /opt/vespa/bin/vespa-start-services
-elif [ "$1" == "configserver,services" ]; then
-  echo "Starting Vespa Config Server and Services..."
-  /opt/vespa/bin/vespa-start-configserver && /opt/vespa/bin/vespa-start-services
-else
-  echo "Allowed arguments to entrypoint are 'configserver', 'services' or 'configserver,services'."
-  exit 1
+echo "Current max_map_count is: $current_max_map"
+
+if [ "$current_max_map" -lt "$required_max_map" ]; then
+    echo "Warning: vm.max_map_count is less than $required_max_map, which may affect performance."
+    echo "Attempting to set vm.max_map_count to $required_max_map..."
+
+    su -c "sysctl -w vm.max_map_count=$required_max_map"
+
+    echo "Done! vm.max_map_count is now set to $required_max_map."
 fi
+
+# Start Vespa's config server and services
+/opt/vespa/bin/vespa-start-configserver && /opt/vespa/bin/vespa-start-services
+
+# Keep the container running
+tail -f /dev/null
