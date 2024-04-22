0b79c8b0e4a3b049c6acfd11f4d80e7074298932
Davy Peter Braun <543614+dheavy@users.noreply.github.com>
2024-04-22 20:53:04 +0200

    WIP

--- deployment/fly.io/index/start-vespa.sh
+++ deployment/fly.io/index/start-vespa.sh
@@ -2,18 +2,26 @@
 
 # Check the current vm.max_map_count
 current_max_map=$(sysctl -n vm.max_map_count)
-required_max_map=262144
-
 echo "Current max_map_count is: $current_max_map"
 
-if [ "$current_max_map" -lt "$required_max_map" ]; then
-    echo "Warning: vm.max_map_count is less than $required_max_map, which may affect performance."
-    echo "Attempting to set vm.max_map_count to $required_max_map..."
+# Start Vespa's config server and services
+# /opt/vespa/bin/vespa-start-configserver && /opt/vespa/bin/vespa-start-services
 
-    su -c "sysctl -w vm.max_map_count=$required_max_map"
+cat <<'EOF'
+:::::::::::::::::::::::::::::::::::::
+::: To finalize the deployment,     :::
+::: ssh into the Vespa container   :::
+::: and run the following command :::
+:::::::::::::::::::::::::::::::::::::
 
-    echo "Done! vm.max_map_count is now set to $required_max_map."
-fi
+# [BEGIN COMMAND]
+su
+echo "vm.max_map_count=262144" > /etc/sysctl.d/60-vespa.conf
+sysctl -p /etc/sysctl.d/60-vespa.conf
+exit
+# [END COMMAND]
+
+EOF
 
 # Start Vespa's config server and services
 /opt/vespa/bin/vespa-start-configserver && /opt/vespa/bin/vespa-start-services
